162e4bfb58fb4eba6f2f26f11b0f46a8ea6587a7
Jordan Harband <ljharb@gmail.com>
2021-09-08 08:43:39 -0700

    [Refactor] remove unnecessary ESM files

--- polyfill.mjs
+++
@@ -1,7 +0,0 @@
-import getPolyfill from './polyfill.js';
-
-export default getPolyfill;
-
-export function then(resolve) {
-	resolve(getPolyfill);
-}
--- test/index.mjs
+++ test/index.mjs
@@ -17,9 +17,9 @@ test('named exports', async (t) => {
 	);
 
 	const { shim, getPolyfill, implementation } = Module;
-	t.equal(await import('reflect.getprototypeof/shim'), shim, 'shim named export matches deep export');
-	t.equal(await import('reflect.getprototypeof/implementation'), implementation, 'implementation named export matches deep export');
-	t.equal(await import('reflect.getprototypeof/polyfill'), getPolyfill, 'getPolyfill named export matches deep export');
+	t.equal((await import('reflect.getprototypeof/shim')).default, shim, 'shim named export matches deep export');
+	t.equal((await import('reflect.getprototypeof/implementation')).default, implementation, 'implementation named export matches deep export');
+	t.equal((await import('reflect.getprototypeof/polyfill')).default, getPolyfill, 'getPolyfill named export matches deep export');
 
 	t.end();
 });
